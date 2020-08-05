6c65f7d5f38b30aa8955eb40f399d0dd746ee110
Can <Can.Karka@Web.de>
2020-08-05 03:16:48 +0200

    modified String class

--- FakeEngine/src/Engine/Core/DataTypes/FakeString.h
+++ FakeEngine/src/Engine/Core/DataTypes/FakeString.h
@@ -2,9 +2,183 @@
 
 #include <cstring>
 
-// TODO
-// - Substr
+// TODO:
+// - Replace
+// - Find
 // - 
+
+// TEMP: move to FakeFileUtils
+static char impl_to_upper_case(char letter)
+	{
+	switch (letter)
+		{
+		case 'a':
+			return 'A';
+
+		case 'b':
+			return 'B';
+
+		case 'c':
+			return 'C';
+
+		case 'd':
+			return 'D';
+
+		case 'e':
+			return 'E';
+
+		case 'f':
+			return 'F';
+
+		case 'g':
+			return 'G';
+
+		case 'h':
+			return 'H';
+
+		case 'i':
+			return 'I';
+
+		case 'j':
+			return 'J';
+
+		case 'k':
+			return 'K';
+
+		case 'l':
+			return 'L';
+
+		case 'm':
+			return 'M';
+
+		case 'n':
+			return 'N';
+
+		case 'o':
+			return 'O';
+
+		case 'p':
+			return 'P';
+
+		case 'q':
+			return 'Q';
+
+		case 'r':
+			return 'R';
+
+		case 's':
+			return 'S';
+
+		case 't':
+			return 'T';
+
+		case 'u':
+			return 'U';
+
+		case 'v':
+			return 'V';
+
+		case 'w':
+			return 'W';
+
+		case 'x':
+			return 'X';
+
+		case 'y':
+			return 'Y';
+
+		case 'z':
+			return 'Z';
+		}
+
+	return letter;
+	}
+
+static char impl_to_lower_case(char letter)
+	{
+	switch (letter)
+		{
+		case 'A':
+			return 'a';
+
+		case 'B':
+			return 'b';
+
+		case 'C':
+			return 'c';
+
+		case 'D':
+			return 'd';
+
+		case 'E':
+			return 'e';
+
+		case 'F':
+			return 'f';
+
+		case 'G':
+			return 'g';
+
+		case 'H':
+			return 'h';
+
+		case 'I':
+			return 'i';
+
+		case 'J':
+			return 'j';
+
+		case 'K':
+			return 'k';
+
+		case 'L':
+			return 'l';
+
+		case 'M':
+			return 'm';
+
+		case 'N':
+			return 'n';
+
+		case 'O':
+			return 'o';
+
+		case 'P':
+			return 'p';
+
+		case 'Q':
+			return 'q';
+
+		case 'R':
+			return 'r';
+
+		case 'S':
+			return 's';
+
+		case 'T':
+			return 't';
+
+		case 'U':
+			return 'u';
+
+		case 'V':
+			return 'v';
+
+		case 'W':
+			return 'w';
+
+		case 'X':
+			return 'x';
+
+		case 'Y':
+			return 'y';
+
+		case 'Z':
+			return 'z';
+		}
+
+	return letter;
+	}
 
 class FakeString
 	{
@@ -57,7 +231,7 @@ class FakeString
 
 		~FakeString()
 			{
-			delete Data;
+			delete[] Data;
 			}
 
 		FakeString &operator=(const FakeString &other)
@@ -94,21 +268,23 @@ class FakeString
 
 		wchar_t *ToWStr()
 			{
-			wchar_t *ret = L"";
+			wchar_t *ret = new wchar_t[Size + 1];
 
 			for (uint32_t i = 0; i < Size; ++i)
-				ret += Data[i];
+				ret[i] = Data[i];
 
+			ret[Size + 1] = '\0';
 			return ret;
 			}
 
 		const wchar_t *ToWStr() const
 			{
-			wchar_t *ret = L"";
+			wchar_t *ret = new wchar_t[Size + 1];
 
 			for (uint32_t i = 0; i < Size; ++i)
-				ret += Data[i];
+				ret[i] = Data[i];
 
+			ret[Size + 1] = '\0';
 			return ret;
 			}
 
@@ -132,9 +308,9 @@ class FakeString
 			return Data[i];
 			}
 
-		FakeString& Append(const char letter)
-		{
-			char* new_data = new char[(size_t)Size + 2]; // +1 for a character and another +1 for the null terminator
+		FakeString &Append(const char letter)
+			{
+			char *new_data = new char[(size_t)Size + 2]; // +1 for a character and another +1 for the null terminator
 			new_data[Size] = letter;
 			new_data[Size + 1] = '\0';
 			if (Data) memcpy(new_data, Data, Size);
@@ -144,12 +320,12 @@ class FakeString
 			++Size;
 
 			return *this;
-		}
+			}
 
-		FakeString& Append(const FakeString &other)
-		{
+		FakeString &Append(const FakeString &other)
+			{
 			uint32_t new_size = Size + other.Size;
-			char* new_data = new char[(size_t)new_size + 1]; // +1 for the null terminator
+			char *new_data = new char[(size_t)new_size + 1]; // +1 for the null terminator
 
 			new_data[new_size] = '\0';
 			if (Data) memcpy(new_data, Data, Size);						// copy existing string data
@@ -160,7 +336,7 @@ class FakeString
 			Size = new_size;
 
 			return *this;
-		}
+			}
 
 		uint32_t FirstIndexOf(const char letter)
 			{
@@ -225,34 +401,34 @@ class FakeString
 			return NULL;
 			}
 
-		FakeString *Split(size_t* outWordCount = 0)
+		FakeString *Split(size_t *outWordCount = 0)
 			{
 			// First get the word count to construct the size of the array
 			// Then iterate through the string and put every word into the array
 
 			uint32_t wordCount = 1;
 			for (uint32_t i = 0; i < Size; ++i)
-			{
+				{
 				if (Data[i] == ' ')
 					++wordCount;
-			}
+				}
 
-			FakeString* result = new FakeString[wordCount];
+			FakeString *result = new FakeString[wordCount];
 
 			uint32_t charIdx = 0;
 			for (uint32_t i = 0; i < wordCount; i++)
-			{
+				{
 				uint32_t wordBeginIdx = charIdx;
 				while (Data[charIdx] != ' ' && Data[charIdx] != '\0')
-				{
+					{
 					charIdx++;
-				}
+					}
 
 				FakeString word = Substr(wordBeginIdx, charIdx);
 				result[i] = word;
 
 				charIdx++;
-			}
+				}
 
 			if (outWordCount)
 				*outWordCount = wordCount;
@@ -280,9 +456,36 @@ class FakeString
 			return FakeString(*this, beginIndex, endIndex);
 			}
 
+		bool Contains(const FakeString &other)
+			{
+			uint32_t i = 0;
+			uint32_t j = 0;
+
+			while (Data[i] != '\0')
+				{
+				if (Data[i] == other[j])
+					{
+					uint32_t init = i;
+					while (Data[i] == other[j] && other[j] != '\0')
+						{
+						j++;
+						i++;
+						}
+
+					if (other[j] == '\0')
+						return true;
+						
+					j = 0;
+					}
+				i++;
+				}
+
+			return false;
+			}
+
 		uint32_t Find(const FakeString &other) const noexcept
 			{
-			
+			// TODO
 
 			return 0;
 			}
@@ -290,6 +493,39 @@ class FakeString
 		FakeString &Replace(const FakeString &find, const FakeString &replaceValue)
 			{
 			// TODO
+
+			return *this;
+			}
+
+		FakeString &ToLowerCase()
+			{
+			for (uint32_t i = 0; i < Size; ++i)
+				Data[i] = impl_to_lower_case(Data[i]);
+
+			return *this;
+			}
+
+		const FakeString &ToLowerCase() const
+			{
+			for (uint32_t i = 0; i < Size; ++i)
+				Data[i] = impl_to_lower_case(Data[i]);
+
+			return *this;
+			}
+
+		FakeString &ToUpperCase()
+			{
+			for (uint32_t i = 0; i < Size; ++i)
+				Data[i] = impl_to_upper_case(Data[i]);
+
+			return *this;
+			}
+
+		const FakeString &ToUpperCase() const
+			{
+			for (uint32_t i = 0; i < Size; ++i)
+				Data[i] = impl_to_upper_case(Data[i]);
+
 			return *this;
 			}
 
@@ -299,6 +535,26 @@ class FakeString
 				printf("%c", Data[i]);
 
 			printf("\n");
+			}
+
+		operator char*()
+			{
+			char *result = new char[Size + 1];
+			for (uint32_t i = 0; i < Size; ++i)
+				result[i] = Data[i];
+
+			result[Size + 1] = '\0';
+			return result;
+			}
+
+		operator const char*() const
+			{
+			char *result = new char[Size + 1];
+			for (uint32_t i = 0; i < Size; ++i)
+				result[i] = Data[i];
+
+			result[Size + 1] = '\0';
+			return result;
 			}
 
 		friend FakeString operator+(FakeString str, const FakeString &other)
--- Sandbox/src/ExampleLayer.h
+++ Sandbox/src/ExampleLayer.h
@@ -7,17 +7,12 @@ class ExampleLayer : public FakeLayer
 	public:
 		virtual void OnAttach() override
 			{
-			FAKE_LOG_INFO("Application starts!");
+		//	FAKE_LOG_INFO("Application starts!");
 
-			FakeString str = "Tes";
-			std::cout << "Old: " << str << "\n";
-			str += 't';
-			std::cout << "New: " << str << "\n\n";
+			FakeString lol = "Test";
 
-			FakeString str2 = "Hello";
-			std::cout << "Old: " << str2 << "\n";
-			str2 += " World!";
-			std::cout << "New: " << str2 << "\n";
+			wchar_t *test = lol.ToWStr();
+
 
 			}
 
